902fa8de3eec1c9a13b3b1b30a2c89787c10b193
Tanya <tanya@policylift.ai>
2026-05-04 18:09:01 +0300

    Add policylift prototype (dashboard + quote packet pages)

---
+++ personal/tanya/policylift-prototype/assets/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_g3lIKD7X3t26GY1HvxOacdd12zeLnQHys3HK9sw1z88'] = {
+    config: {"analytics":{"endpoint":"/i/v0/e/"},"autocaptureExceptions":false,"autocapture_opt_out":false,"captureDeadClicks":true,"capturePerformance":{"network_timing":true,"web_vitals":true,"web_vitals_allowed_metrics":null},"conversations":false,"defaultIdentifiedOnly":true,"elementsChainAsString":true,"errorTracking":{"autocaptureExceptions":false,"suppressionRules":[]},"hasFeatureFlags":true,"heatmaps":true,"logs":{"captureConsoleLogs":false},"productTours":false,"sdkVersion":{"requested":"1"},"sessionRecording":false,"supportedCompression":["gzip","gzip-js"],"surveys":false,"token":"phc_g3lIKD7X3t26GY1HvxOacdd12zeLnQHys3HK9sw1z88"},
+    siteApps: []
+  }
+})();
